ecc4e12b056cf27d00310693205d3ef8592f2032
any-stone <kuxa.business@gmail.com>
2022-07-03 15:01:51 -0600

    added JS drag and drop functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const fill = document.querySelector('fill')
-const empty = document.querySelectorAll('empty')
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
 
 fill.addEventListener('dragstart', dragStart)
 fill.addEventListener('dragend', dragEnd)
@@ -12,26 +12,29 @@ for(const empty of empties) {
 }
 
 function dragStart() {
-    console.log('drag start')
+    this.className += ' hold'
+    setTimeout(() => this.className = 'invisible', 0)
 }
 
 function dragEnd() {
-    console.log('drag end')
+    this.className = 'fill'
 }
 
-function dragOver() {
-    console.log('drag over')
+function dragOver(e) {
+    e.preventDefault()
 }
 
-function dragEnter() {
-    console.log('drag enter')
+function dragEnter(e) {
+    e.preventDefault()
+    this.className += ' hovered'
 }
 
 function dragLeave() {
-    console.log('drag leave')
+    this.className = 'empty'
 }
 
 function dragDrop() {
-    console.log('drag drop')
+    this.className = 'empty'
+    this.append(fill)
 }
 
